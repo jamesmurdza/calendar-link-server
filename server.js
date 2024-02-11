60fdf8350259925ca55267fcd4f9ef4b7c86f7ff
James Murdza <james@jamesmurdza.com>
2024-02-11 12:51:52 -0800

    Add rate limiting.

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 import express from 'express';
 import cors from 'cors';
+import rateLimit from 'express-rate-limit';
 
 import textEndpoint from './routes/text.js';
 
 const app = express();
 const port = 3000;
+
+// Rate limiting setup
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // limit each IP to 100 requests per windowMs
+});
+
+// Apply to all requests
+app.use(limiter);
 
 app.use(cors());
 
